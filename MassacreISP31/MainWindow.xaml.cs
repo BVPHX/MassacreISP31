a1885ed587314d913c7c8e92dc06e4dc0552fda1
ИСП-31 <-31@LAB17-12>
2022-04-09 12:31:50 +0300

    Gatova

--- MassacreISP31/MainWindow.xaml.cs
+++ MassacreISP31/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace MassacreISP31
         {
             InitializeComponent();
         }
+        public void Victim_Output(object sender, SelectionChangedEventArgs e)
+        {
+            switch (victimList.SelectedIndex)
+            {
+                case 0:
+                    string str = ((ListBoxItem)victimList.SelectedItem).Content.ToString();
+                    OutWindow.Victim victim = new OutWindow.Victim(str, "100", "Дробовик", 1);
+                    OutWindow da = new OutWindow(victim);
+                    da.ShowDialog();
+                    break;
+                case 1:
+                    str = ((ListBoxItem)victimList.SelectedItem).Content.ToString();
+                    victim = new OutWindow.Victim(str, "200", "Оружие для убийства: Арбалет ", 2);
+                    da = new OutWindow(victim);
+                    da.ShowDialog();
+                    break;
+                case 2:
+                    str = ((ListBoxItem)victimList.SelectedItem).Content.ToString();
+                    victim = new OutWindow.Victim(str, "50", "провод", 3);
+                    da = new OutWindow(victim);
+                    da.ShowDialog();
+                    break;
+                case 3:
+                    str = ((ListBoxItem)victimList.SelectedItem).Content.ToString();
+                    victim = new OutWindow.Victim(str, "150", "Ботинок (немытый)", 4);
+                    da = new OutWindow(victim);
+                    da.ShowDialog();
+                    break;
+                case 4:
+                    str = ((ListBoxItem)victimList.SelectedItem).Content.ToString();
+                    victim = new OutWindow.Victim(str, "400", "Беговая дорожка", 5);
+                    da = new OutWindow(victim);
+                    da.ShowDialog();
+                    break;
+            }
+        }
+
+        private void Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
+
+
 }
---
+++ MassacreISP31/OutWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MassacreISP31
+{
+    /// <summary>
+    /// Логика взаимодействия для OutWindow.xaml
+    /// </summary>
+    public partial class OutWindow : Window
+    {
+        public OutWindow(Victim victim)
+        {InitializeComponent();
+            lbVictimHP.Content = victim.Health;
+            lbVictimName.Content = victim.VictimName;
+            lbWeapon.Content = victim.WeaponName;
+            meWeaponImage.Source = victim.WeaponImage.Source;
+            
+        }
+        public class Victim
+        {
+            public string VictimName { get; set; }
+            public string Health { get; set; }
+            public string WeaponName { get; set; }
+            public Image WeaponImage { get; set; }
+            public Victim(string victimname, string health, string weaponname, int imnum)
+            {
+                VictimName = victimname;
+                Health = health;
+                WeaponName = weaponname;
+                WeaponImage = new Image();
+                WeaponImage.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + @"\" + imnum.ToString() + ".jpg"));
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
